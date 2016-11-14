39ba2b7629bc1cae6598eb856b74c921a291aacf
Noitidart <noitidart@gmail.com>
2016-11-14 11:32:06 -0800

    1.0b.initial * Still setting it up, working on bringing in exe

---
+++ README.md
@@ -0,0 +1,10 @@
+## Installation
+Firefox (Desktop + Android) - [AMO :: Trigger](https://addons.mozilla.org/en-US/firefox/addon/trigger/)
+
+## About
+Trigger is a browser extension. It gives you the power to create a collection of hotkeys that will work even if the browser is not focused. The browser must be running though.
+
+This also works in Firefox for Android. Hotkeys here are variations of swipes, such as 5-finger swipe from left.
+
+## Credits
+* Freepik for Icon [(source)](http://www.flaticon.com/free-icon/antenna_212180)
---
+++ src/webextension/scripts/background.js
@@ -0,0 +1,537 @@
+var nub = {
+	self: {
+		id: chrome.runtime.id,
+		version: chrome.runtime.getManifest().version,
+		chromemanifestkey: 'trigger'
+	},
+	browser: {
+		name: getBrowser().name.toLowerCase(),
+		version: getBrowser().version
+	},
+	path: {
+		// webext relative paths
+		images: 'images/',
+		fonts: 'styles/fonts/',
+		pages: 'pages/',
+		scripts: 'scripts/',
+		styles: 'styles/',
+		exe: 'exe/',
+		// non-webext paths - SPECIAL prefixed with underscore - means it is from chrome://nub.addon.id/content/
+		_webextension: 'webextension/',
+		// chrome path versions set after this block
+		chrome: {}
+	},
+	nativemessaging: { // only used by firefox
+		manifest_json: {
+		  name: 'trigger', // i use this as child entry for windows registry entry, so make sure this name is compatible with injection into windows registry link39191
+		  description: 'Platform helper for Trigger',
+		  path: undefined, // set by `getNativeMessagingInfo` in bootstrap
+		  type: 'stdio',
+		  allowed_extensions: [ chrome.runtime.id ]
+		}
+	},
+	stg: {
+		// defaults - keys that present in here during `preinit` are fetched on startup
+			// 3 types
+				// prefs are prefeixed with "pref_"
+				// mem are prefeixed with "mem_" - mem stands for extension specific "cookies"/"system memory"
+				// filesystem-like stuff is prefixied with "fs_"
+		mem_lastversion: '-1', // indicates not installed - the "last installed version"
+	}
+};
+
+// set chrome:// paths in nub.path
+for (var pathkey in nub.path) {
+	if (pathkey == 'chrome') continue;
+	var prefix = 'chrome://' + nub.self.chromemanifestkey + '/content/';
+	var suffix;
+	if (pathkey[0] == '_') pathkey = pathkey.substr(1);
+	if (nub.path['_' + pathkey]) {
+		// its a chrome path only
+		suffix = nub.path['_' + pathkey];
+	} else {
+		suffix = 'webextension/' + nub.path[pathkey];
+	}
+	nub.path.chrome[pathkey] = prefix + suffix;
+}
+
+var gExeComm;
+var gPortsComm = new Comm.server.webextports();
+var gBsComm = new Comm.client.webext();
+
+var callInAPort = Comm.callInX2.bind(null, gPortsComm, null);
+var callInExe = Comm.callInX2.bind(null, 'gExeComm', null, null); // cannot use `gExeComm` it must be `"gExeComm"` as string because `gExeComm` var was not yet assigned
+var callInBootstrap = Comm.callInX2.bind(null, gBsComm, null, null);
+var callInMainworker = Comm.callInX2.bind(null, gBsComm, 'callInMainworker', null);
+var callInNative; // set in preinit, if its android, it is eqaul to callInMainworker, else it is equal to callInExe
+
+// start - init
+preinit();
+
+// never retries - only one special case of retry is after auto install native messaging
+function preinit(aIsRetry) {
+
+	var promiseallarr = [];
+	/*
+	 promises in promiseallarr when get rejected, reject with:
+		{
+			reason: string;enum[STG_CONNECT, EXE_CONNECT, EXE_MISMATCH]
+			text: string - non-localized associated text to show - NOT formated text. this is something i would insert into the template shown
+			data: object - only if EXE_MISMATCH - keys: exeversion
+		}
+	*/
+	// fetch storage
+	promiseallarr.push(new Promise(function(resolve, reject) {
+		storageCall('local', 'get', Object.keys(nub.stg))
+		.then(function(stgeds) {
+			for (var key in stgeds) {
+				nub.stg[key] = stgeds[key];
+			}
+			resolve();
+		})
+		.catch(function(err) {
+			reject(err);
+		});
+	}));
+
+	// get platform info - and startup exe (desktop) or mainworker (android)
+	// set callInNative
+	promiseallarr.push(new Promise(function(resolve, reject) {
+		// start async-proc133934
+		var getPlat = function() {
+			chrome.runtime.getPlatformInfo(gotPlat);
+		};
+
+		var gotPlat = function(platinfo) {
+			console.log('platinfo:', platinfo);
+			nub.platform = platinfo;
+
+			callInNative = nub.platform.os == 'android' ? callInMainworker : callInExe;
+
+			// resolve('ok platinfo got')
+			startNativeHost();
+		};
+
+		var startNativeHost = function(aIsRetry) {
+			if (nub.platform.os == 'android') {
+				callInBootstrap('startupMainworker', { path:'chrome://profilist/content/webextension/scripts/mainworker.js' });
+				// worker doesnt start till first call, so just assume it connected
+				verifyNativeHost();
+			} else {
+				gExeComm = new Comm.server.webextexe(
+					'profilist',
+					function() {
+						// exe connected
+						verifyNativeHost();
+					},
+					function(aErr) {
+						// exe failed to connect
+						console.error('failed to connect to exe, aErr:', aErr);
+						if (aErr) aErr = aErr.toString(); // because at the time of me writing this, Comm::webext.js does not give an error reason fail, i tried but i couldnt get the error reason, it is only logged to console
+
+						if (!aIsRetry) {
+							// try reinstalling
+							if (nub.browser.name == 'firefox') {
+									callInBootstrap('installNativeMessaging', { nativemessaging:nub.nativemessaging, path:nub.path }, function(aInstallFailed) {
+										if (!aInstallFailed) startNativeHost(true);
+										else verifyNativeHost({ reason:'EXE_CONNECT', text:aErr + '\n\n' + aInstallFailed });
+									});
+							} else {
+								// TODO: present dialog which will download the installer zip from within the addon, with a "RETRY" button so they can click after installing
+							}
+						} else {
+							verifyNativeHost({ reason:'EXE_CONNECT', text:aErr });
+						}
+					}
+				);
+			}
+		};
+
+		var verifyNativeHost = function(aRejectObj) {
+			// last step of async-proc - responsible for calling `resolve` or `reject`
+			console.log('in verifyNativeHost');
+			if (aRejectObj) {
+				reject(aRejectObj);
+			} else {
+				// verifies the native host version matches that of the extension
+				if (nub.platform.os == 'android') {
+					resolve('ok platinfo got AND nativehost (mainworker) started up');
+				} else {
+					callInExe('getExeVersion', undefined, function(exeversion) {
+						var extversion = nub.self.version;
+						if (exeversion === extversion) {
+							resolve('ok platinfo got AND exe started up AND exe version matches extension version');
+						} else {
+							// TODO: xhr exe and send it to exe to self update
+							reject({ reason:'EXE_MISMATCH', data:{ exeversion:exeversion } });
+						}
+					});
+				}
+			}
+		};
+
+		getPlat();
+		// end async-proc133934
+	}));
+
+	Promise.all(promiseallarr)
+	.then(function(valarr) {
+		console.log('valarr:', valarr);
+		// ok `preinit` completed successfully
+		init();
+	})
+	.catch(function onPreinitFailed(err) {
+		console.error('onPreinitFailed, err:', err);
+
+		// build body, based on err.reason, with localized template and with err.text and errex
+		var body, bodyarr;
+		switch (err.reason) {
+			// case 'STG_CONNECT': // let default handler take care of this
+			// 		//
+			// 	break;
+			// case 'EXE_CONNECT':
+			//
+			// 		bodyarr = [chrome.i18n.getMessage('startupfailed_execonnect') + ' ' + (err.text || chrome.i18n.getMessage('startupfailed_unknown'))]
+			// 		if (errex) bodyarr[0] += ' ' + errex.toString();
+			//
+			// 	break;
+			// case 'EXE_MISMATCH':
+			//
+			// 		// build howtofixstr
+			// 		var extversion = nub.self.version;
+			// 		var exeversion = err.data.exeversion;
+			// 		console.log('going to isSemVer', extversion, exeversion);
+			// 		console.log('semver:', isSemVer(extversion, '>' + exeversion));
+			// 		var howtofixstr = isSemVer(extversion, '>' + exeversion) ? chrome.i18n.getMessage('startupfailed_exemismatch_howtofix1') : chrome.i18n.getMessage('startupfailed_exemismatch_howtofix2');
+			// 		bodyarr = [ chrome.i18n.getMessage('startupfailed_exemismatch', [exeversion, extversion, howtofixstr]) ];
+			// 		if (errex) bodyarr[0] += ' ' + errex.toString();
+			//
+			// 	break;
+			default:
+				var txt = '';
+				if (err && err.text) txt += err.text;
+				if (txt && errex) txt += '\n';
+				if (errex) txt += errex;
+
+				bodyarr = [ txt || chrome.i18n.getMessage('startupfailed_unknown') ];
+		}
+		var body = chrome.i18n.getMessage('startupfailed_body', bodyarr);
+
+		// show error to user
+		callInBootstrap('showSystemAlert', { title:chrome.i18n.getMessage('startupfailed_title'), body:body });
+	});
+}
+
+function init() {
+	// after receiving nub
+	console.log('in init, nub:', nub);
+
+	startupBrowserAction();
+
+	var lastversion = nub.stg.mem_lastversion;
+	if (lastversion === '-1') {
+		// installed / first run
+		console.error('FIRST RUN');
+		storageCall('local', 'set', { mem_lastversion:nub.self.version })
+		.then(a=>console.log('set, nub.stg:', nub.stg));
+	} else if (lastversion !== nub.self.version) {
+		// downgrade or upgrade
+		if (isSemVer(nub.self.version, '>' + lastversion)) {
+			// upgrade
+			console.error('UPDGRADE');
+		} else {
+			// downgrade
+			console.error('DOWNGRADE');
+		}
+		storageCall('local', 'set', { mem_lastversion:nub.self.version })
+		.then(a=>console.log('set, nub.stg:', nub.stg));
+	} // else if (lastversion === nub.self.version) { } // browser startup OR enabled after having disabled
+
+	// ensure `mem_faking` status is refelected
+	if (nub.stg.mem_faking) {
+		setFaking(true);
+	} else {
+		setFaking(false);
+	}
+}
+
+function uninit() {
+	// from IRC on 093016 - no need to unregister these ports, they are done for me on addon sutdown
+	// gPortsComm.unregister();
+	// gExeComm.unregister();
+}
+// end - init
+
+// start - browseraction
+function startupBrowserAction() {
+	// browser_action/chrome.browserAction is not supported on Android, so tell bootstrap to inject action item to NativeWindow.menu
+	if (nub.platform.os == 'android') {
+		callInBootstrap('startupAndroid', {
+			browseraction: {
+				name: chrome.i18n.getMessage('browseraction_title'),
+				// iconpath: chrome.runtime.getURL('images/icon.svg')
+				checkable: false
+			}
+		});
+	} else {
+		chrome.browserAction.setBadgeBackgroundColor({color:'#7DCE8D'});
+		chrome.browserAction.onClicked.addListener(onBrowserActionClicked);
+	}
+}
+function onBrowserActionClicked() {
+	console.log('opening menu.html now');
+	addTab(chrome.runtime.getURL(nub.path.pages + 'app.html'));
+}
+// end - browseraction
+
+async function fetchData(aArg={}) {
+	let { hydrant, nub:wantsnub } = aArg;
+	// xprefs means xpcom prefs
+
+	let data = {};
+
+	let basketmain = new PromiseBasket;
+
+	if (wantsnub) data.nub = nub;
+
+	if (hydrant) {
+		data.hydrant = {};
+		if ('stg' in hydrant) {
+			basketmain.add(
+				storageCall('local', 'get', Object.keys(hydrant.stg)),
+				stgeds => data.hydrant.stg = stgeds
+				// stgeds => { console.log('got stgeds:', stgeds); data.stg = stgeds; }
+			);
+		}
+		// if ('xprefs' in hydrant) { // xpcom_prefs
+		// 	basketmain.add(
+		// 		new Promise( resolve=>callInBootstrap('getXPrefs', { nametypes:{  'geo.provider.testing':'boolean', 'geo.wifi.uri':'string'  } }, xprefs => resolve(xprefs)) ),
+		// 		xprefvals => data.hydrant.xprefs=xprefvals
+		// 		// xprefvals => { console.error('got xprefvals in bg:', xprefvals); data.xprefs=xprefvals; }
+		// 	)
+		// }
+	}
+
+	await basketmain.run();
+	return data;
+}
+
+function setFaking(aNewStatus) {
+	// aNewStatus - boolean; - true for on, false for off
+	switch (nub.browser.name) {
+		case 'firefox':
+
+				if (aNewStatus) {
+					let { pref_lat:lat, pref_lng:lng } = nub.stg;
+					let geojson = { location:{ lat, lng }, accuracy:4000 };
+					let geouri = 'data:,' + encodeURIComponent(JSON.stringify(geojson));
+					let xprefvals = { 'geo.wifi.uri':geouri, 'geo.provider.testing':true };
+					callInBootstrap('setXPrefs', { namevals:xprefvals } );
+				} else {
+					let xprefvals = { 'geo.wifi.uri':null, 'geo.provider.testing':null };
+					callInBootstrap('setXPrefs', { namevals:xprefvals });
+				}
+
+			break;
+		default:
+			throw new Error(nub.browser.name + ' browser not supported!')
+	}
+
+	// set badge
+	if (nub.platform.os != 'android') {
+		if (aNewStatus) {
+			chrome.browserAction.setBadgeText({text:chrome.i18n.getMessage('on')});
+			browserActionSetTitle(chrome.i18n.getMessage('browseraction_title_on'));
+		} else {
+			chrome.browserAction.setBadgeText({text:''});
+			browserActionSetTitle(chrome.i18n.getMessage('browseraction_title'));
+		}
+	} else {
+		if (aNewStatus) {
+			callInBootstrap('browserActionUpdate', { checked:true, checkable:true, name:chrome.i18n.getMessage('browseraction_title_on') });
+		} else {
+			callInBootstrap('browserActionUpdate', { checked:false, checkable:false, name:chrome.i18n.getMessage('browseraction_title') });
+		}
+	}
+}
+
+// start - polyfill for android
+function browserActionSetTitle(title) {
+	if (nub.platform.os != 'android') {
+		chrome.browserAction.setTitle({title});
+	} else {
+		callInBootstrap('update');
+	}
+}
+function browserActionSetBadgeText(text) {
+
+}
+function addTab(url) {
+	if (chrome.tabs && chrome.tabs.create) {
+		chrome.tabs.create({ url:url });
+	} else {
+		// its android
+		callInBootstrap('addTab', { url:url });
+	}
+}
+function reuseElseAddTab(url) {
+	// find tab by url, if it exists focus its window and tab and the reuse it. else add tab
+}
+// end - polyfill for android
+
+// start - cmn
+// rev3 - not yet comit - https://gist.github.com/Noitidart/bcb964207ac370d3301720f3d5c9eb2b
+var _storagecall_pendingset = {};
+var _storagecall_callid = 1;
+function storageCall(aArea, aAction, aKeys, aOptions) {
+	if (typeof(aArea) == 'object') ({ aArea, aAction, aKeys, aOptions } = aArea);
+	// because storage can fail, i created this, which goes until it doesnt fail
+
+	// aAction - string;enum[set,get,clear,remove]
+	// aKeys -
+		// if aAction "clear" then ignore
+		// if aAction "remove" then string/string[]
+		// if aAction "get" then null/string/string[]
+		// if aAction "set" then object
+	// aOptions - object
+		// maxtries - int;default:0 - set to 0 if you want it to try infinitely
+		// timebetween - int;default:50 - milliseconds
+
+	aOptions = aOptions ? aOptions : {};
+	const maxtries = aOptions.maxtries || 0;
+	const timebetween = aOptions.timebetween || 50;
+
+	const callid = _storagecall_callid++; // the id of this call to `storageCall` // only used for when `aAction` is "set"
+
+	if (aAction == 'set') {
+		// see if still trying to set any of these keys
+		for (var setkey in aKeys) {
+			_storagecall_pendingset[setkey] = callid;
+		}
+	}
+	return new Promise(function(resolve, reject) {
+		// start asnc-proc49399
+		var trycnt = 0;
+
+		var call = function() {
+			switch (aAction) {
+				case 'clear':
+						chrome.storage[aArea][aAction](check);
+					break;
+				case 'set':
+						// special processing
+						// start - block-link3191
+						// make sure that each this `callid` is still responsible for setting in `aKeys`
+						for (var setkey in aKeys) {
+							if (_storagecall_pendingset[setkey] !== callid) {
+								delete aKeys[setkey];
+							}
+						}
+						// end - block-link3191
+						if (!Object.keys(aKeys).length) resolve(); // no longer responsible, as another call to set - with the keys that this callid was responsible for - has been made, so lets say it succeeded // i `resolve` and not `reject` because, say i was still responsible for one of the keys, when that completes it will `resolve`
+						else chrome.storage[aArea][aAction](aKeys, check);
+					break;
+				default:
+					chrome.storage[aArea][aAction](aKeys, check);
+			}
+		};
+
+		var check = function(arg1) {
+			if (chrome.runtime.lastError) {
+				if (!maxtries || trycnt++ < maxtries) setTimeout(call, timebetween);
+				else reject(chrome.runtime.lastError); // `maxtries` reached
+			} else {
+				switch (aAction) {
+					case 'clear':
+					case 'remove':
+							// callback `check` triggred with no arguments
+							resolve();
+					case 'set':
+							// callback `check` triggred with no arguments - BUT special processing
+
+							// race condition I THINK - because i think setting storage internals is async - so what if another call came in and did the set while this one was in between `call` and `check`, so meaningi t was processing - and then this finished processing AFTER a new call to `storageCall('', 'set'` happend
+							// copy - block-link3191
+							// make sure that each this `callid` is still responsible for setting in `aKeys`
+							for (var setkey in aKeys) {
+								if (_storagecall_pendingset[setkey] !== callid) {
+									delete aKeys[setkey];
+								}
+							}
+							// end copy - block-link3191
+
+							// remove keys from `_storagecall_pendingset`
+							for (var setkey in aKeys) {
+								// assuming _storagecall_pendingset[setkey] === callid
+								delete _storagecall_pendingset[setkey];
+							}
+
+							// SPECIAL - udpate nub.stg
+							if (typeof(nub) == 'object' && nub.stg) {
+								for (let setkey in aKeys) {
+									if (setkey in nub.stg) nub.stg[setkey] = aKeys[setkey];
+								}
+							}
+
+							resolve(aKeys);
+						break;
+					case 'get':
+							// callback `check` triggred with 1 argument
+							var stgeds = arg1;
+							resolve(stgeds);
+						break;
+				}
+				resolve(stgeds);
+			}
+		};
+
+		call();
+		// end asnc-proc49399
+	});
+}
+
+// rev2 - https://gist.github.com/Noitidart/59ee6c306fa493a4f35fb122bcf13e99
+function getBrowser() {
+	function getBrowserInner() {
+		// http://stackoverflow.com/a/2401861/1828637
+	    var ua= navigator.userAgent, tem,
+	    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+	    if(/trident/i.test(M[1])){
+	        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+	        return 'IE '+(tem[1] || '');
+	    }
+	    if(M[1]=== 'Chrome'){
+	        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+	        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+	    }
+	    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+	    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+	    return M.join(' ');
+	}
+
+	var name_version_str = getBrowserInner();
+	var split = name_version_str.split(' ');
+	var version = split.pop();
+	var name = split.join(' ');
+	return {
+		name: name,
+		version: version
+	};
+}
+
+class PromiseBasket {
+	constructor() {
+		this.promises = [];
+		this.thens = [];
+	}
+	add(aAsync, onThen) {
+		// onThen is optional
+		this.promises.push(aAsync);
+		this.thens.push(onThen);
+	}
+	async run() {
+		let results = await Promise.all(this.promises);
+		results.forEach((r, i)=>this.thens[i] ? this.thens[i](r) : null);
+		return results;
+	}
+}
+// end - cmn
